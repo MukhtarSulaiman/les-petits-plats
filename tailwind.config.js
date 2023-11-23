50a198cf47cfc5f26eeee1cd68ecff3b39809ed6
Mukhtar Sulaiman <mukhtar.sulaiman.pro@gmail.com>
2023-11-23 17:18:07 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{html,js}"],
+	theme: {
+		colors: {},
+		fontFamily: {
+			sans: ['sans-serif'],
+      		serif: ['serif'],
+		},
+		extend: {
+			spacing: {},
+			borderRadius: {}
+		},
+	},
+	plugins: [],
+};
