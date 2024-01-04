cdaca4faa81aa4a314cf9ba645d08ce43672593b
Mukhtar SULAIMAN <84712917+MukhtarSulaiman@users.noreply.github.com>
2024-01-04 19:00:23 +0100

    Tags can be selected

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -12,6 +12,7 @@ const iconChevronUp = document.querySelector('.datalist-container .icon-chevron-
 const iconChevronDown = document.querySelector('.datalist-container .icon-chevron-down');
 const datalist = document.querySelector('.datalist-container ul');
 const totaleRecipesAvailable = document.querySelector('.totale-recipes-available');
+const ingredientTagsContainer = document.getElementById('ingredient-tags-container');
 
 // Displaying filtered elements
 const displayFilteredElements = (recipes) => {
@@ -119,6 +120,29 @@ const selectIngredients = () => {
 
         element.addEventListener('mouseleave', (event) => {
             event.target.classList.remove('bg-primary');
+        });
+
+        element.addEventListener('click', (event) => {
+            // console.log(ingredientTagsContainer)
+            // console.log(event.target)
+
+            event.target.classList.add('!list-none', '!p-0')
+
+            const div = document.createElement('div');
+            const i = document.createElement('i');
+
+            i.textContent = '✕';
+            i.classList.add('cursor-pointer');
+
+            div.appendChild(event.target);
+            div.appendChild(i);
+
+            div.classList.add('flex', 'justify-between', 'items-center', 'bg-primary', 'p-3', 'mb-1', 'rounded-lg');
+            ingredientTagsContainer.classList.add('w-48', 'mt-12');
+
+            ingredientTagsContainer.appendChild(div);
+
+            datalist.parentElement.classList.toggle('!block');
         });
     });
 };
